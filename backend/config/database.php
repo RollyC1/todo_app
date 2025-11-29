a1c41f302e463bb3141df46a07062fd07f4f49e4
RollyC1 <rollycastrobonifacio@gmail.com>
2025-11-29 14:02:29 +0800

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -83,9 +83,9 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+       'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL', env('DB_URL')),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
